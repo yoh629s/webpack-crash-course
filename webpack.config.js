375dba55a0155b4e0f66bfe9dfc2208ab45b70a0
yoh629s <yoh629s@gmail.com>
2019-04-28 19:22:56 +0900

    install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,8 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-      }
+      },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist')
     }
+  }
