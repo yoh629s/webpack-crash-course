c4af13e20cf443210bb4698ac09b3b3f52bbe7fb
yoh629s <yoh629s@gmail.com>
2019-04-29 15:34:17 +0900

    uglifyjs追加
    productionモード時はコンソールログを出力しないように修正

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './style.css'
+import './style.scss'
+
+console.log("I am printing logs.")
 
 ReactDOM.render(
     <div>Hello, React!</div>,
