8d1cdedc1ae0f5a903e6eb99dca74e82e360b6d7
yoh629s <yoh629s@gmail.com>
2019-05-04 15:34:32 +0900

    eslint追加

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,13 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: "pre",
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+
+      },
+      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: "babel-loader"
